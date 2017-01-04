e5f3c580f2e37816ebe3f18467a5358aca492ef7
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 17:23:24 -0500

    populations table migration

--- db/migrate/20170104215217_create_populations.rb
+++ db/migrate/20170104215217_create_populations.rb
@@ -1,4 +1,10 @@
 class CreatePopulations < ActiveRecord::Migration[5.0]
   def change
+    create_table :populations do |t|
+      t.integer :warriors
+      t.integer :farmers
+      t.integer :priests
+      t.integer :tribe_id
+    end
   end
 end
