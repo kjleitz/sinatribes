2912308a9b15a7d031247822683be817a3db8e95
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 18:07:55 -0500

    association macros

--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -1,3 +1,13 @@
 class Tribe < ActiveRecord::Base
-
+  belongs_to :user
+  belongs_to :religion
+  has_one :population
+  has_one :warriors, through: :population
+  has_one :farmers, through: :population
+  has_one :priests, through: :population
+  has_many :messengers
+  has_many :tribe_buildings
+  has_many :buildings, through: :tribe_buildings
+  has_many :tribe_resources
+  has_many :resources, through: :tribe_resources
 end
