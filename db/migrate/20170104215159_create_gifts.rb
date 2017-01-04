8c5c3c6dcebc1b7773befc61a76073803bea3283
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 17:43:59 -0500

    gifts table migration

--- db/migrate/20170104215159_create_gifts.rb
+++ db/migrate/20170104215159_create_gifts.rb
@@ -1,4 +1,11 @@
 class CreateGifts < ActiveRecord::Migration[5.0]
   def change
+    create_table :gifts do |t|
+      t.integer :money, default: 0
+      t.integer :warriors, default: 0
+      t.integer :resource_amount, default: 0
+      t.integer :resource_id
+      t.integer :messenger_id
+    end
   end
 end
