f2b62bee1df9b5000ac90ecdd5c0aa90ed543098
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 17:30:53 -0500

    tribe_resources join table migration

--- db/migrate/20170104215251_create_tribe_resources.rb
+++ db/migrate/20170104215251_create_tribe_resources.rb
@@ -1,4 +1,8 @@
 class CreateTribeResources < ActiveRecord::Migration[5.0]
   def change
+    create_table :tribe_resources do |t|
+      t.integer :tribe_id
+      t.integer :resource_id
+    end
   end
 end
