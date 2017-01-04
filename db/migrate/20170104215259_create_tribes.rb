460fa32a70a071c56ecfa9441135c07c4b3e005d
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 17:01:06 -0500

    tribes table migration

--- db/migrate/20170104215259_create_tribes.rb
+++ db/migrate/20170104215259_create_tribes.rb
@@ -1,4 +1,12 @@
 class CreateTribes < ActiveRecord::Migration[5.0]
   def change
+    create_table :tribes do |t|
+      t.string :name
+      t.integer :land
+      t.integer :money
+      t.integer :technology
+      t.integer :user_id
+      t.integer :religion_id
+    end
   end
 end
