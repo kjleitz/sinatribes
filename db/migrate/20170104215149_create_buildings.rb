d90451232476edde0167d51797fed3606542b20f
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 17:26:24 -0500

    buildings table migration

--- db/migrate/20170104215149_create_buildings.rb
+++ db/migrate/20170104215149_create_buildings.rb
@@ -1,4 +1,10 @@
 class CreateBuildings < ActiveRecord::Migration[5.0]
   def change
+    create_table :buildings do |t|
+      t.string :name
+      t.integer :price
+      t.integer :resource_amount
+      t.integer :resource_id
+    end
   end
 end
