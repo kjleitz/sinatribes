330a9778692addd748c050cd1aa35f9512754734
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 17:28:08 -0500

    tribe_buildings join table migration

--- db/migrate/20170104215243_create_tribe_buildings.rb
+++ db/migrate/20170104215243_create_tribe_buildings.rb
@@ -1,4 +1,8 @@
 class CreateTribeBuildings < ActiveRecord::Migration[5.0]
   def change
+    create_table :tribe_buildings do |t|
+      t.integer :tribe_id
+      t.integer :building_id
+    end
   end
 end
