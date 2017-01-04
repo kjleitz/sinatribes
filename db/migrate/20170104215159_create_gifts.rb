35cd7a4e385df73ddcfd75d24383cdf1f4e928de
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 17:47:29 -0500

    added 'accepted' column to gift

--- db/migrate/20170104215159_create_gifts.rb
+++ db/migrate/20170104215159_create_gifts.rb
@@ -4,6 +4,7 @@ class CreateGifts < ActiveRecord::Migration[5.0]
       t.integer :money, default: 0
       t.integer :warriors, default: 0
       t.integer :resource_amount, default: 0
+      t.boolean :accepted, default: false
       t.integer :resource_id
       t.integer :messenger_id
     end
