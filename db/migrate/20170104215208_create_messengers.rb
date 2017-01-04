c1aaffc0f8cf973d6f1d219223334cc72ce6f509
Keegan Leitz <kjleitz@gmail.com>
2017-01-04 17:34:39 -0500

    messengers table migration

--- db/migrate/20170104215208_create_messengers.rb
+++ db/migrate/20170104215208_create_messengers.rb
@@ -1,4 +1,10 @@
 class CreateMessengers < ActiveRecord::Migration[5.0]
   def change
+    create_table :messengers do |t|
+      t.string :message
+      t.integer :home_id
+      t.integer :destination_id
+      t.timestamps
+    end
   end
 end
