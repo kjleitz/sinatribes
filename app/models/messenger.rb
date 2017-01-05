dffcf0a984d285af1d76b506221f91c69da8ae3e
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 08:40:24 -0500

    messenger rejecting methods

--- app/models/messenger.rb
+++ app/models/messenger.rb
@@ -2,4 +2,12 @@ class Messenger < ActiveRecord::Base
   has_one :gift
   belongs_to :home, foreign_key: :home_id, class_name: 'Tribe'
   belongs_to :destination, foreign_key: :destination_id, class_name: 'Tribe'
+
+  def rejected?
+    self.rejected?
+  end
+
+  def reject
+    self.update(rejected: true)
+  end
 end
