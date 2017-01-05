48b90569f82d847c402cb4a7d5069f1d6aeb24ee
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 09:16:36 -0500

    methods for adding content to gifts

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -9,4 +9,19 @@ class Gift < ActiveRecord::Base
   def accept
     self.update(accepted: true)
   end
+
+  def add_money(amt)
+    self.update(money: amt) if self.messenger.tribe.pay(amt)
+  end
+
+  def add_warriors(amt)
+    self.update(warriors: amt) if self.messenger.tribe.lose_warriors(amt)
+  end
+
+  def add_resource(amt, resource_name)
+    if self.resource = self.messenger.tribe.resources.find_by(name: resource_name)
+      self.resource_amount = amt
+      self.save
+    end
+  end
 end
