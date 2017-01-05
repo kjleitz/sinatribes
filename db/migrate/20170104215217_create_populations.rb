78d1e13607f75cb288fe6d3b1b1b8a390c59ea85
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 11:14:31 -0500

    set defaults for population

--- db/migrate/20170104215217_create_populations.rb
+++ db/migrate/20170104215217_create_populations.rb
@@ -1,9 +1,9 @@
 class CreatePopulations < ActiveRecord::Migration[5.0]
   def change
     create_table :populations do |t|
-      t.integer :warriors
-      t.integer :farmers
-      t.integer :priests
+      t.integer :warriors, default: 0
+      t.integer :farmers, defualt: 10
+      t.integer :priests, default: 1
       t.integer :tribe_id
     end
   end
