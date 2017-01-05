98bba4a14d93a2caab318dc050a727395cb18c0f
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 08:24:51 -0500

    association macros

--- app/models/messenger.rb
+++ app/models/messenger.rb
@@ -1,3 +1,5 @@
 class Messenger < ActiveRecord::Base
-
+  has_one :gift
+  belongs_to :home, foreign_key: :home_id, class_name: 'Tribe'
+  belongs_to :destination, foreign_key: :destination_id, class_name: 'Tribe'
 end
