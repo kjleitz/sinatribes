ab98a853a3e9943fe9d0a7ebcadd3798b800b97d
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 10:41:33 -0500

    defaults for land, money, tech

--- db/migrate/20170104215259_create_tribes.rb
+++ db/migrate/20170104215259_create_tribes.rb
@@ -2,9 +2,9 @@ class CreateTribes < ActiveRecord::Migration[5.0]
   def change
     create_table :tribes do |t|
       t.string :name
-      t.integer :land
-      t.integer :money
-      t.integer :technology
+      t.integer :land, default: 1
+      t.integer :money, default: 1000
+      t.integer :technology, default: 1
       t.integer :user_id
       t.integer :religion_id
     end
