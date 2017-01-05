4e1d85698f2f03fa0c09ebe97d00b44953c7a355
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 15:09:28 -0500

    typo: defualt -> default

--- db/migrate/20170104215217_create_populations.rb
+++ db/migrate/20170104215217_create_populations.rb
@@ -2,7 +2,7 @@ class CreatePopulations < ActiveRecord::Migration[5.0]
   def change
     create_table :populations do |t|
       t.integer :warriors, default: 0
-      t.integer :farmers, defualt: 10
+      t.integer :farmers, default: 10
       t.integer :priests, default: 1
       t.integer :tribe_id
     end
