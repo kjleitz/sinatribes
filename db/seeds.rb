205e62615786f65fa5f9f2ed5c61bbabfad9007b
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 10:49:26 -0500

    user seeds

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+User.create(
+  username: "test",
+  email: "test@blah.com"
+  password: "test"
+)
+User.create(
+  username: "billiam",
+  email: "bill@bill.com"
+  password: "password1"
+)
+User.create(
+  username: "sarah",
+  email: "me@timsister.com"
+  password: "weddingslol"
+)
+User.create(
+  username: "peabody",
+  email: "smart@dog.com"
+  password: "isapeople"
+)
+User.create(
+  username: "deadpool",
+  email: "jokes@you.com"
+  password: "fourthwall"
+)
