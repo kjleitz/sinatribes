12a09d9b45198132f8105416543b2080d734a1ca
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 08:38:35 -0500

    gift accepting methods

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,4 +1,12 @@
 class Gift < ActiveRecord::Base
   belongs_to :resource
   belongs_to :messenger
+
+  def accepted?
+    self.accepted
+  end
+
+  def accept
+    self.update(accepted: true)
+  end
 end
