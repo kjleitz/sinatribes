4ac297c13f4445dd3e0a14e06f50e1b0e6995548
Keegan Leitz <kjleitz@gmail.com>
2017-01-06 09:24:11 -0500

    timestamps for tribes table

--- db/migrate/20170104215259_create_tribes.rb
+++ db/migrate/20170104215259_create_tribes.rb
@@ -7,6 +7,7 @@ class CreateTribes < ActiveRecord::Migration[5.0]
       t.integer :technology, default: 1
       t.integer :user_id
       t.integer :religion_id
+      t.timestamps
     end
   end
 end
