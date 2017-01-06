4c18c38c4d2b782d8e39eb30fa399129561d7e1c
Keegan Leitz <kjleitz@gmail.com>
2017-01-06 13:49:13 -0500

    explanatory comment

--- app/controllers/concerns/helper.rb
+++ app/controllers/concerns/helper.rb
@@ -5,6 +5,8 @@ module Helper
   end
 
   def current_user
+    # the "if session[:id]" portion seems redundant, but it prevents an
+    # unnecessary database query. BITCHIN'.
     @current_user ||= User.find_by(id: session[:id]) if session[:id]
   end
 
