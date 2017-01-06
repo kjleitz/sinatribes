ecdd1d928cece942d88a6a1edf29f16974241b6b
Keegan Leitz <kjleitz@gmail.com>
2017-01-06 18:00:09 -0500

    validate signup params, create user, sign in/redirect to index if successful, retry if not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,33 @@ class UsersController < ApplicationController
   end
 
   get "/signup" do
-    redirect to("/user/#{current_user.username}") if logged_in?
+    redirect to("/") if logged_in?
     erb :"users/signup"
   end
 
   post "/signup" do
+    if !User.validate_username(params[:username])
+      flash[:message] = "Usernames may only contain letters, numbers, and underscores, and be less than 80 characters in length."
+    elsif !User.validate_email(params[:email])
+      flash[:message] = "Please enter a valid email address."
+    elsif User.find_by(username: params[:username])
+      flash[:message] = "Sorry, the username '#{params[:username]}' is taken. Please try something else."
+    elsif User.find_by(email: params[:email])
+      flash[:message] = "There's already an account using this email address. Would you like to <a href=\"/login\">log in</a> instead?"
+    elsif params[:password].empty?
+      flash[:message] = "Password cannot be blank."
+    elsif user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      flash[:message] = "Successfully signed up!"
+      session[:user_id] = user.id
+    else
+      flash[:message] = "Something went wrong. Please try again."
+    end
 
+    redirect user ? to("/") : to("/signup")
   end
 
   get "/login" do
-    redirect to("/user/#{current_user.username}") if logged_in?
+    redirect to("/") if logged_in?
     erb :"users/login"
   end
 
