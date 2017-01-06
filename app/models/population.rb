0c9597efaf51703cbf34f3696882944b9336a2f2
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 19:27:19 -0500

    #happiness for gauging overall happiness of the tribe's population

--- app/models/population.rb
+++ app/models/population.rb
@@ -4,4 +4,13 @@ class Population < ActiveRecord::Base
   def total
     self.warriors + self.farmers + self.priests
   end
+
+  def happiness
+    happy_val = 0
+    happy_val += self.tribe.buildings.count
+    happy_val += self.priests
+    happy_val += (self.warriors / 2)
+    happy_val -= self.farmers if (total / self.tribe.land) > 50
+  end
+
 end
