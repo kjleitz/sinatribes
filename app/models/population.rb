ac0c17adf5668c9973f78f8b1280454fa84e99b4
Keegan Leitz <kjleitz@gmail.com>
2017-01-05 19:28:50 -0500

    added #density for population density

--- app/models/population.rb
+++ app/models/population.rb
@@ -5,12 +5,16 @@ class Population < ActiveRecord::Base
     self.warriors + self.farmers + self.priests
   end
 
+  def density
+    total / self.tribe.land
+  end
+
   def happiness
     happy_val = 0
     happy_val += self.tribe.buildings.count
     happy_val += self.priests
     happy_val += (self.warriors / 2)
-    happy_val -= self.farmers if (total / self.tribe.land) > 50
+    happy_val -= self.farmers if density > 50
   end
 
 end
