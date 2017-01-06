bdd021263f6e6475926c73bfffc6c3a81094d300
Keegan Leitz <kjleitz@gmail.com>
2017-01-06 14:03:56 -0500

    changed session[:id] to session[:user_id] for no particular reason

--- app/controllers/concerns/helper.rb
+++ app/controllers/concerns/helper.rb
@@ -7,7 +7,7 @@ module Helper
   def current_user
     # the "if session[:id]" portion seems redundant, but it prevents an
     # unnecessary database query. BITCHIN'.
-    @current_user ||= User.find_by(id: session[:id]) if session[:id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
 end
