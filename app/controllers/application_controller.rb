ded656241a9fb877c21a031f6d8e3583d2edf787
Keegan Leitz <kjleitz@gmail.com>
2017-01-06 11:40:47 -0500

    added some config settings for main app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < Sinatra::Base
-
+  configure do
+    set :public_folder, "public"
+    set :views, "app/views"
+    enable :sessions
+    set :session_secret, "after all these implements and texts designed by intellects"
+    use Rack::Flash
+  end
 end
