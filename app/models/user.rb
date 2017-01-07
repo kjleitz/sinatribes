845c62981642773d6f173026fa78068ad0a5fd6a
Keegan Leitz <kjleitz@gmail.com>
2017-01-07 12:11:32 -0500

    validate presence of password_digest, not password (returns nil with has_secure_password)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :username
   validates_presence_of :email
-  validates_presence_of :password
+  validates_presence_of :password_digest
 
   def self.validate_username(username)
     username.match(/\A\w{1,80}\z/)
