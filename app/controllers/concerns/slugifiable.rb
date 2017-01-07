32d4e71d6266767d5bbf11e7356fbe9e34a39e44
Keegan Leitz <kjleitz@gmail.com>
2017-01-07 10:40:33 -0500

    adds id to slug for guaranteed uniqueness

--- app/controllers/concerns/slugifiable.rb
+++ app/controllers/concerns/slugifiable.rb
@@ -2,7 +2,7 @@ module Slugifiable
 
   # you can use attribute=:username if you need to do a username, etc.
   def slug(attribute=:name)
-    self.send(attribute).downcase.gsub(/\W+/, "-")
+    self.send(attribute).downcase.gsub(/\W+/, "-") + "-" + self.id.to_s
   end
 
   def self.find_by_slug(slug, attribute=:name)
