c706773a1f2196e14478637b6914ccdd0a52acf5
Keegan Leitz <kjleitz@gmail.com>
2017-01-07 10:43:32 -0500

    usernames are unique and I like the simplicity of being able to type them where they're slugged

--- app/controllers/concerns/slugifiable.rb
+++ app/controllers/concerns/slugifiable.rb
@@ -2,7 +2,8 @@ module Slugifiable
 
   # you can use attribute=:username if you need to do a username, etc.
   def slug(attribute=:name)
-    self.send(attribute).downcase.gsub(/\W+/, "-") + "-" + self.id.to_s
+    slug = self.send(attribute).downcase.gsub(/\W+/, "-")
+    slug + "-" + self.id.to_s unless attribute == :username
   end
 
   def self.find_by_slug(slug, attribute=:name)
