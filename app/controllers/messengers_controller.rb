2a7d85b3e6ca322be15d29f502df1fcc51df1e08
Keegan Leitz <kjleitz@gmail.com>
2017-01-08 13:42:00 -0500

    can't send messengers to your own tribe

--- app/controllers/messengers_controller.rb
+++ app/controllers/messengers_controller.rb
@@ -3,8 +3,13 @@ class MessengersController < ApplicationController
   get "/messengers/new/:id" do |id|
     if destination = Tribe.find_by(id: id)
       if tribe = current_user.tribe
-        @messenger = Messenger.create(tribe: tribe, destination: destination)
-        erb :"messengers/new"
+        if tribe != destination
+          @messenger = Messenger.create(tribe: tribe, destination: destination)
+          erb :"messengers/new"
+        else
+          flash[:message] = "This is your tribe. You can't send a messenger to your own tribe, silly!"
+          redirect to("/tribes/#{tribe.slug}")
+        end
       else
         flash[:message] = "You need to set an <strong>active tribe</strong> before you can send a messenger to another tribe."
         redirect to("/")
