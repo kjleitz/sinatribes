e7010cb8f70be99180925558889fbd0ba9a26318
Keegan Leitz <kjleitz@gmail.com>
2017-01-08 11:46:58 -0500

    route for creating new messengers between a user's active tribe and a destination tribe

--- app/controllers/messengers_controller.rb
+++ app/controllers/messengers_controller.rb
@@ -1,3 +1,17 @@
 class MessengersController < ApplicationController
 
+  get "/messengers/new/:id" do |id|
+    if destination = Tribe.find_by(id: id)
+      if tribe = current_user.tribe
+        @messenger = Messenger.create(tribe: tribe, destination: destination)
+        erb :"messengers/new"
+      else
+        flash[:message] = "You need to set an <strong>active tribe</strong> before you can send a messenger to another tribe."
+      end
+    else
+      flash[:message] = "You can't send a messenger to a tribe that doesn't exist!"
+    end
+    redirect to("/")
+  end
+
 end
