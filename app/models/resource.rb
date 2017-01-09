0844244dfd1d26feca5ac90affc137eb6067f0da
Keegan Leitz <kjleitz@gmail.com>
2017-01-09 12:36:03 -0500

    method for 'discovering' resources (meant for when buying land)

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,9 @@
 class Resource < ActiveRecord::Base
   belongs_to :tribe
   belongs_to :gift
+
+  def self.discover
+    self.create(name: self.all.reject { |r| r.name == "cloth" }.sample.name)
+  end
+
 end
