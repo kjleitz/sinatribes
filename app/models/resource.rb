7eb5a56d1f74a1955152bf4aa29f9d2265143036
Keegan Leitz <kjleitz@gmail.com>
2017-01-09 12:51:43 -0500

    methods for creating random resources from their categories

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -27,4 +27,16 @@ class Resource < ActiveRecord::Base
     self.create(name: DISCOVERABLES.sample)
   end
 
+  def self.harvest
+    self.create(name: HARVESTABLES.sample)
+  end
+
+  def self.mine
+    self.create(name: MINEABLES.sample)
+  end
+
+  def self.manufacture
+    self.create(name: MANUFACTURABLES.sample)
+  end
+
 end
