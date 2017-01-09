d9361773e20d10051cede514a6c389ab3ae81db6
Keegan Leitz <kjleitz@gmail.com>
2017-01-09 12:58:51 -0500

    default action types for certain buildings

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,12 @@
 class Building < ActiveRecord::Base
   has_many :tribe_buildings
   has_many :tribes, through: :tribe_buildings
+
+  ACTIONS = {
+    "factory" => "manufacture",
+    "farm" => "harvest",
+    "mine" => "mine",
+    "barracks" => "train"
+  }
+
 end
