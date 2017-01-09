5280f13e90acc80e571a351fa96d191a1ffdac94
Keegan Leitz <kjleitz@gmail.com>
2017-01-09 13:01:32 -0500

    add building's default action after creation (if it has one)

--- app/models/building.rb
+++ app/models/building.rb
@@ -2,11 +2,17 @@ class Building < ActiveRecord::Base
   has_many :tribe_buildings
   has_many :tribes, through: :tribe_buildings
 
+  after_create :initialize_action
+
   ACTIONS = {
     "factory" => "manufacture",
     "farm" => "harvest",
     "mine" => "mine",
     "barracks" => "train"
   }
+
+  def initialize_action
+    self.update(action: action) if action = ACTIONS[self.name]
+  end
 
 end
