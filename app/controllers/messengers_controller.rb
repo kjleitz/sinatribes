f8c730d05eb06b9130fa9353ef2e335fecfe52eb
Keegan Leitz <kjleitz@gmail.com>
2017-01-08 20:32:11 -0500

    post to /messengers to 'send' (create) a messenger

--- app/controllers/messengers_controller.rb
+++ app/controllers/messengers_controller.rb
@@ -19,4 +19,31 @@ class MessengersController < ApplicationController
     end
   end
 
+  post "/messengers" do
+    if destination = Tribe.find_by(id: params[:destination_id])
+      if current_user.tribe == destination
+        flash[:message] = "No loopholes! Sending a message to your own tribe is weird and unnatural and I don't like it, so stop trying. Oh god, PLEASE stop trying!"
+        redirect to("/tribes/#{destination.slug}")
+      else
+        messenger = Messenger.create(
+          message: params[:message],
+          tribe: current_user.tribe,
+          destination: destination,
+          gift: Gift.create(params[:gift])
+        )
+        messenger.gift.attach_resource(params[:resource_name])
+        if messenger && messenger.gift
+          flash[:message] = "Successfully sent the messenger!"
+          redirect to("/messengers/#{current_user.tribe.slug}")
+        else
+          flash[:message] = "Something went wrong. Please try again!"
+          redirect to("/messengers/new/#{params[:destination_id]}")
+        end
+      end
+    else
+      flash[:message] = "You can't send a messenger to a tribe that doesn't exist!"
+      redirect to("/")
+    end
+  end
+
 end
