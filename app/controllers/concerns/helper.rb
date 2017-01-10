922a9094df7ce53458fe55c0cef758ac3158a0da
Keegan Leitz <kjleitz@gmail.com>
2017-01-10 14:46:22 -0500

    keyword conflict fix: break -> breaker

--- app/controllers/concerns/helper.rb
+++ app/controllers/concerns/helper.rb
@@ -10,8 +10,8 @@ module Helper
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  def format_time(time, break=false)
-    time.strftime("%A, %b %d#{break ? "<br>" : " "}%I:%M %p")
+  def format_time(time, breaker=false)
+    time.strftime("%A, %b %d#{breaker ? "<br>" : " "}%I:%M %p")
   end
 
 end
