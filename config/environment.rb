432418fdee8ffbe97af3221e1265bf9a9681f9f6
Keegan Leitz <kjleitz@gmail.com>
2017-01-10 23:23:19 -0500

    db url, maybe that will fix all my problems

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
-  :database => "development"
+  :database => ENV["DATABASE_URL"]
 )
 
 require_all 'app'
