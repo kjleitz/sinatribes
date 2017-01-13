3e6478b6903fc151a65a640dadd38b71ac52ebb2
Keegan Leitz <kjleitz@gmail.com>
2017-01-13 02:19:29 -0500

    30 second global wait time

--- app/models/building.rb
+++ app/models/building.rb
@@ -6,7 +6,7 @@ class Building < ActiveRecord::Base
   after_create :initialize_used_time
 
   # Remember for later: This is global. It affects all users after one building use.
-  BUILDING_WAIT_PERIOD = 10
+  BUILDING_WAIT_PERIOD = 30
   ACTIONS = {
     "factory" => "manufacture",
     "farm" => "harvest",
